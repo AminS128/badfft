903e3ba9bfc76d9da8d470e727b20d4a66e9b48b
Amin Smith <aminsmith128@gmail.com>
2025-04-09 10:13:18 -0400

    Update badfft.js
    add output shifting, rename takefft() to badfft(), remove unnecessary code from fft.js

--- badfft.js
+++ badfft.js
@@ -1,9 +1,4 @@
-
-
-
-
-
-function takefft(data){
+function badfft(data){
     const f = new FFT(data.length)
     const out = f.createComplexArray()
     f.realTransform(out, data)
@@ -16,8 +11,18 @@ function takefft(data){
             0.5 * Math.log(out[i*2] * out[i*2] + out[i*2+1] * out[i*2+1] + 0.001)// no log (0)
     }
 
-    // console.log(output)
-    return output
+    const pW = Math.trunc(Math.sqrt(data.length))
+
+    const shifted = new Float64Array(output.length);
+    const offs = pW / 2;
+    for (let y = 0; y < pW; y++) {
+        for (let x = 0; x < pW; x++) {
+            let newX = (x + offs) % pW;
+            let newY = (y + offs) % pW;
+            shifted[newX + newY * pW] = output[x + y * pW];
+        }
+    }
+    return shifted;
 }
 
 
@@ -62,28 +67,13 @@ function FFT(size) {
     this._inv = 0;
   }
   
-  FFT.prototype.fromComplexArray = function fromComplexArray(complex, storage) {
-    var res = storage || new Array(complex.length >>> 1);
-    for (var i = 0; i < complex.length; i += 2)
-      res[i >>> 1] = complex[i];
-    return res;
-  };
-  
   FFT.prototype.createComplexArray = function createComplexArray() {
     const res = new Array(this._csize);
     for (var i = 0; i < res.length; i++)
       res[i] = 0;
     return res;
   };
-  
-  FFT.prototype.toComplexArray = function toComplexArray(input, storage) {
-    var res = storage || this.createComplexArray();
-    for (var i = 0; i < res.length; i += 2) {
-      res[i] = input[i >>> 1];
-      res[i + 1] = 0;
-    }
-    return res;
-  };
+
   
   FFT.prototype.completeSpectrum = function completeSpectrum(spectrum) {
     var size = this._csize;
@@ -93,18 +83,7 @@ function FFT(size) {
       spectrum[size - i + 1] = -spectrum[i + 1];
     }
   };
-  
-  FFT.prototype.transform = function transform(out, data) {
-    if (out === data)
-      throw new Error('Input and output buffers must be different');
-  
-    this._out = out;
-    this._data = data;
-    this._inv = 0;
-    this._transform4();
-    this._out = null;
-    this._data = null;
-  };
+ 
   
   FFT.prototype.realTransform = function realTransform(out, data) {
     if (out === data)
@@ -116,209 +95,6 @@ function FFT(size) {
     this._realTransform4();
     this._out = null;
     this._data = null;
-  };
-  
-  FFT.prototype.inverseTransform = function inverseTransform(out, data) {
-    if (out === data)
-      throw new Error('Input and output buffers must be different');
-  
-    this._out = out;
-    this._data = data;
-    this._inv = 1;
-    this._transform4();
-    for (var i = 0; i < out.length; i++)
-      out[i] /= this.size;
-    this._out = null;
-    this._data = null;
-  };
-  
-  // radix-4 implementation
-  //
-  // NOTE: Uses of `var` are intentional for older V8 version that do not
-  // support both `let compound assignments` and `const phi`
-  FFT.prototype._transform4 = function _transform4() {
-    var out = this._out;
-    var size = this._csize;
-  
-    // Initial step (permute and transform)
-    var width = this._width;
-    var step = 1 << width;
-    var len = (size / step) << 1;
-  
-    var outOff;
-    var t;
-    var bitrev = this._bitrev;
-    if (len === 4) {
-      for (outOff = 0, t = 0; outOff < size; outOff += len, t++) {
-        const off = bitrev[t];
-        this._singleTransform2(outOff, off, step);
-      }
-    } else {
-      // len === 8
-      for (outOff = 0, t = 0; outOff < size; outOff += len, t++) {
-        const off = bitrev[t];
-        this._singleTransform4(outOff, off, step);
-      }
-    }
-  
-    // Loop through steps in decreasing order
-    var inv = this._inv ? -1 : 1;
-    var table = this.table;
-    for (step >>= 2; step >= 2; step >>= 2) {
-      len = (size / step) << 1;
-      var quarterLen = len >>> 2;
-  
-      // Loop through offsets in the data
-      for (outOff = 0; outOff < size; outOff += len) {
-        // Full case
-        var limit = outOff + quarterLen;
-        for (var i = outOff, k = 0; i < limit; i += 2, k += step) {
-          const A = i;
-          const B = A + quarterLen;
-          const C = B + quarterLen;
-          const D = C + quarterLen;
-  
-          // Original values
-          const Ar = out[A];
-          const Ai = out[A + 1];
-          const Br = out[B];
-          const Bi = out[B + 1];
-          const Cr = out[C];
-          const Ci = out[C + 1];
-          const Dr = out[D];
-          const Di = out[D + 1];
-  
-          // Middle values
-          const MAr = Ar;
-          const MAi = Ai;
-  
-          const tableBr = table[k];
-          const tableBi = inv * table[k + 1];
-          const MBr = Br * tableBr - Bi * tableBi;
-          const MBi = Br * tableBi + Bi * tableBr;
-  
-          const tableCr = table[2 * k];
-          const tableCi = inv * table[2 * k + 1];
-          const MCr = Cr * tableCr - Ci * tableCi;
-          const MCi = Cr * tableCi + Ci * tableCr;
-  
-          const tableDr = table[3 * k];
-          const tableDi = inv * table[3 * k + 1];
-          const MDr = Dr * tableDr - Di * tableDi;
-          const MDi = Dr * tableDi + Di * tableDr;
-  
-          // Pre-Final values
-          const T0r = MAr + MCr;
-          const T0i = MAi + MCi;
-          const T1r = MAr - MCr;
-          const T1i = MAi - MCi;
-          const T2r = MBr + MDr;
-          const T2i = MBi + MDi;
-          const T3r = inv * (MBr - MDr);
-          const T3i = inv * (MBi - MDi);
-  
-          // Final values
-          const FAr = T0r + T2r;
-          const FAi = T0i + T2i;
-  
-          const FCr = T0r - T2r;
-          const FCi = T0i - T2i;
-  
-          const FBr = T1r + T3i;
-          const FBi = T1i - T3r;
-  
-          const FDr = T1r - T3i;
-          const FDi = T1i + T3r;
-  
-          out[A] = FAr;
-          out[A + 1] = FAi;
-          out[B] = FBr;
-          out[B + 1] = FBi;
-          out[C] = FCr;
-          out[C + 1] = FCi;
-          out[D] = FDr;
-          out[D + 1] = FDi;
-        }
-      }
-    }
-  };
-  
-  // radix-2 implementation
-  //
-  // NOTE: Only called for len=4
-  FFT.prototype._singleTransform2 = function _singleTransform2(outOff, off,
-      step) {
-  const out = this._out;
-  const data = this._data;
-  
-  const evenR = data[off];
-  const evenI = data[off + 1];
-  const oddR = data[off + step];
-  const oddI = data[off + step + 1];
-  
-  const leftR = evenR + oddR;
-  const leftI = evenI + oddI;
-  const rightR = evenR - oddR;
-  const rightI = evenI - oddI;
-  
-  out[outOff] = leftR;
-  out[outOff + 1] = leftI;
-  out[outOff + 2] = rightR;
-  out[outOff + 3] = rightI;
-  };
-  
-  // radix-4
-  //
-  // NOTE: Only called for len=8
-  FFT.prototype._singleTransform4 = function _singleTransform4(outOff, off,
-      step) {
-  const out = this._out;
-  const data = this._data;
-  const inv = this._inv ? -1 : 1;
-  const step2 = step * 2;
-  const step3 = step * 3;
-  
-  // Original values
-  const Ar = data[off];
-  const Ai = data[off + 1];
-  const Br = data[off + step];
-  const Bi = data[off + step + 1];
-  const Cr = data[off + step2];
-  const Ci = data[off + step2 + 1];
-  const Dr = data[off + step3];
-  const Di = data[off + step3 + 1];
-  
-  // Pre-Final values
-  const T0r = Ar + Cr;
-  const T0i = Ai + Ci;
-  const T1r = Ar - Cr;
-  const T1i = Ai - Ci;
-  const T2r = Br + Dr;
-  const T2i = Bi + Di;
-  const T3r = inv * (Br - Dr);
-  const T3i = inv * (Bi - Di);
-  
-  // Final values
-  const FAr = T0r + T2r;
-  const FAi = T0i + T2i;
-  
-  const FBr = T1r + T3i;
-  const FBi = T1i - T3r;
-  
-  const FCr = T0r - T2r;
-  const FCi = T0i - T2i;
-  
-  const FDr = T1r - T3i;
-  const FDi = T1i + T3r;
-  
-  out[outOff] = FAr;
-  out[outOff + 1] = FAi;
-  out[outOff + 2] = FBr;
-  out[outOff + 3] = FBi;
-  out[outOff + 4] = FCr;
-  out[outOff + 5] = FCi;
-  out[outOff + 6] = FDr;
-  out[outOff + 7] = FDi;
   };
   
   // Real input radix-4 implementation
